94b3e1602424753356aae484f7f62adf7a5735ec
AdhiRaj08 <113873429+AdhiRaj08@users.noreply.github.com>
2024-09-17 09:44:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,46 +5,38 @@ This application defines routes for handling user requests from JS.
 Below are the necessary libraries and models used in the project
 """
 
-from flask import Flask, url_for, redirect, render_template, Response, request, jsonify
+from flask import Flask, url_for, redirect, render_template, Response, request, jsonify, json
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 import goslate
-from langdetect import detect
-from textblob import TextBlob
-import langid
-from collections import Counter
 import pdfplumber       
-from transformers import TFAutoModelForQuestionAnswering, AutoTokenizer, pipeline
+import nltk
 import pickle
-import numpy as np
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Embedding, Dense
 from tensorflow.keras.models import load_model
 import numpy as np
-import nltk
-import utkit
-import polygot
-from polyglot.detect import Detector
-from nltk.tokenize import word_tokenize
+from keras.models import load_model
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+import torch
+import PyPDF2
+import gensim.corpora as corpora
+from gensim.models import LdaModel
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-import gensim
-from gensim import corpora
-import fitz
-import cv2
+from nltk.tokenize import word_tokenize
+from googletrans import Translator
+from langchain_community.document_loaders import PyPDFLoader
 
 # Global Variables and Models
-text_content = ""                                           # Stores text content extracted from PDF
+text_content = ""                                          
 tokens = ""                                                 # Stores tokenized words
-model = load_model('next_words.h5')                         # Stores LSTM model
-cnn_model = tf.keras.models.load_model("resnet.h5")         # Stores R-CNN model 
-tokenizer = pickle.load(open('token.pkl', 'rb'))            # Stores pre-trained tokenizer
-
-# Details regarding the Hugging Face Transformer used
-model_name = "deepset/tinyroberta-squad2"
-nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+from flask_cors import CORS  # Import CORS
+nltk.download('stopwords')
 app = Flask(__name__)
+CORS(app)  
+tokenizer = AutoTokenizer.from_pretrained("models/saved_model/")
+model = AutoModelForQuestionAnswering.from_pretrained("models/saved_model/")
+
+lstm_model = load_model('models/lstm_model/next_words.h5')
+lstm_tokenizer = pickle.load(open('models/lstm_model/token.pkl', 'rb'))
 
 @app.route("/")
 def home():
@@ -54,18 +46,12 @@ def home():
     :return: A rendered HTML template for the home page.
     """
    return render_template("bo.html")
+stop_words = set(stopwords.words('english'))
+def preprocess(doc):
+    return [word for word in doc.lower().split() if word not in stop_words]
 
-@app.errorhandler(404)
-def page_not_found(error):
-    """
-    Error handler for 404 Not Found.
-
-    :param error: The error object.
-    :return: A rendered HTML template for the 404 error page.
-    """
-    return render_template('404.html'), 404
-
-@app.route('/submit', methods = ['POST', 'GET'])
+    
+@app.route('/upload', methods = ['POST'])
 def submit():
     """
     Accepts Form inputs and utilize them in generating answer using sota models
@@ -73,61 +59,68 @@ def submit():
     :return: The Corpus/text fetched as per the scope of question in JSON Format.
     """
     if request.method == 'POST':
-        text_content = request.form['pdf'] # corresponds to name
-        question = request.form['search']
-        resized_images = []
-
-        # Converting PDF into stack of Images
-        c = 0
-        with pdfplumber.open('/content/national_security_strategy.pdf') as pdf:
-            text_content = ""
+        content = request.files['pdf']
+        document = ""
+        if content.filename == '':
+            return jsonify({"error": "No selected file"}), 400
+    
+        with pdfplumber.open(content) as pdf:
             for page in pdf.pages:
-                pixmap = page.get_pixmap()
-                image_file = f"page_{c + 1}.png"
-                c = c+1
-                pixmap.writePNG(image_file)
-
-        pdf.close()
-
-        # HCR using R-CNN model to deal with pdf comprised of handwritten text images
-        for img in pixmap:
-            resized_img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_AREA)
-            resized_images.append(resized_img)
-            resized_images = np.array(resized_images)
-            features = cnn_model.predict(resized_images)
-            text_content.append()
-
-
-        # Perform language detection using Polyglot
-        def detect_majority_language(doc):
-            detector = Detector(doc)
-            detected_language = detector.language.code
-            return detected_language
-
-        # Perform language conversion using Goslate API
-        gs = goslate.Goslate()
-        gs.translate(question, detect_majority_language(text_content[:300]))
-
-
+                document += page.extract_text()
+               
+        question = request.form.get('search', None)
+        
+        if not question:
+            return jsonify({'error': 'No question provided'}), 400
+    
+        # Perform language conversion using Googlelens API
+        translator = Translator()
+        question = translator.translate(question, dest='en').text
 
         # Translated Questions are sent into QnA system for information retrival
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        model = TFAutoModelForQuestionAnswering.from_pretrained(model_name)
+        inputs = tokenizer.encode_plus(question, document, add_special_tokens=True,  
+            max_length=512,  # Truncate to the max length (depends on model)
+            padding='max_length',  # Pad to the max length if needed
+            truncation=True,  # Truncate if the text is too long
+            return_tensors="pt"  # Return as PyTorch tensors return_tensors="pt")
+        )
+        # Get answer
+        outputs = model(**inputs)
+        answer_start = torch.argmax(outputs.start_logits)
+        answer_end = torch.argmax(outputs.end_logits) + 1
+        answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(inputs["input_ids"][0][answer_start:answer_end]))
+        answer = answer.replace(question, '').strip()
+       
+        if answer.strip() == "<s>":
+            answer = "No answer found."
 
-        inputs = tokenizer(question, text_content, return_tensors='tf', max_length=512, truncation=True)
-        outputs = model(inputs)
-        start_scores, end_scores = outputs.start_logits, outputs.end_logits
 
-        start_index = tf.argmax(start_scores, axis=1).numpy()[0]
-        end_index = (tf.argmax(end_scores, axis=1) + 1).numpy()[0]
+        
+         #Topic Modelling using LDA
+        processed_docs = [preprocess(doc) for doc in document]
 
-        answer_tokens = tokenizer.convert_ids_to_tokens(inputs["input_ids"].numpy()[0][start_index:end_index])
-        answer_content = tokenizer.convert_tokens_to_string(answer_tokens)
+        dictionary = corpora.Dictionary(processed_docs)
+        corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+        num_topics = 4
+        lda_model = LdaModel(corpus, num_topics=num_topics, id2word=dictionary, passes=10)
 
-        return render_template('bo.html', answer_content = answer_content)
+        topics_payload = {}
+        for idx, topic in lda_model.print_topics(-1):
+            topics_payload[f"Topic {idx}"] = topic
 
+        # Convert the payload to JSON
+        topics_json = json.dumps(topics_payload)
+        topics_dict = json.loads(topics_json)
+        extracted_topics = {}
 
-@app.route("/get_suggestions", methods = 'POST')
+        for topic, words in topics_dict.items():
+            word_components = [word.split('*')[1].strip().strip('"') for word in words.split(' + ')]
+            first_four_words = word_components[:4]
+            extracted_topics[topic] = first_four_words
+
+        return jsonify({'answer':answer, 'topics':extracted_topics['Topic 0']})
+
+@app.route("/get_suggestions", methods=['POST'])
 def next_word_pred():
     """
     Accepts Form input(Question) as being typed by user through AJAX mechansim
@@ -136,50 +129,22 @@ def next_word_pred():
     :return: List of Suggestion in JSON Format.
     """
     
-    text = request.get_json()
-    text = text.split(" ")
-    text = text[-3:]
+    data = request.get_json()
+    text = data.get('value', '')  # Get the value associated with the key 'value'
 
-    s = tokenizer.texts_to_sequences([text_content])
+    words = text.split(" ")
+    words = " ".join(words[-3:])
+
+    s = lstm_tokenizer.texts_to_sequences([words])
     s = np.array(s)
-    preds = np.argmax(model.predict(s))
+    preds = np.argmax(lstm_model.predict(s))
     predicted = ""
 
-    for key, value in tokenizer.word_index.items():
+    for key, value in lstm_tokenizer.word_index.items():
         if value == preds:
             predicted += key
 
     return jsonify(result = predicted)
-
-@app.route("/upload")
-def upload():
-    """
-    Accepts Form input(PDF) and utilize it for generating the top 5 Subject Topics in given corpus
-    Utlizing LSTM model
-
-    :return: List of popular Topics.
-    """
-    punctuation = [',', '.', ';', ':', '!', '?', "'"]
-    tokens = [word for word in tokens if word not in punctuation and word not in stopwords.words('english') and not word.isdigit()]
-    lemmatizer = WordNetLemmatizer()
-    lemmatized_tokens = [lemmatizer.lemmatize(word) for word in tokens]
-
-    document = ' '.join([word for word in lemmatized_tokens if len(word) > 2])
-    
-    texts = [doc.split() for doc in [document]]
-    dictionary = corpora.Dictionary(texts)
-    corpus = [dictionary.doc2bow(text) for text in texts]
-    lda = gensim.models.ldamodel.LdaModel(corpus, num_topics=3, id2word=dictionary, passes=15)
-    document_topics = lda.get_document_topics(corpus[0], minimum_probability=0.0)
-    sorted_topics = sorted(document_topics, key=lambda x: x[1], reverse=True)
-    top_5_topics = sorted_topics[:3]
-    topics = lda.show_topics(num_topics=1, num_words=5, formatted=False)
-    top = []
-    for topic_id, topic in topics:
-        word_freq = {word: freq for word, freq in topic}
-        top.append({word for word, freq in topic})
-    
-    return render_template('bo.html', topics_content = top) 
 
 
 @app.route("/acc")
@@ -196,7 +161,6 @@ def tell():
     """
     return render_template("about.html")
 
-
-
 if __name__ == "__main__":
     app.run(debug = True)
+
